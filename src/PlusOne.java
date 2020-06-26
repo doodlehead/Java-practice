e839b9342332b19ebb1cce5beba2451561dc1a66
Frank <frankyu1999@gmail.com>
2020-06-25 23:44:09 -0400

    V2 of PlusOne, less code, less memory usage

--- src/PlusOne.java
+++ src/PlusOne.java
@@ -1,7 +1,8 @@
 //Runtime 0ms
 //Memory Usage 39.9 MB
 public class PlusOne {
-    public int[] plusOne(int[] digits) {
+    //V1
+    public int[] plusOneV1(int[] digits) {
         if (digits.length == 1 && digits[0] == 9) return new int[]{ 1, 0 };
         int carry;
 
@@ -37,6 +38,37 @@ public class PlusOne {
                 carry = 0;
             }
         }
+        return digits;
+    }
+
+    //V2 - A smarter way to doing it
+    //Runtime: 0ms
+    //Memory Usage: 37.7 MB
+    public int[] plusOne(int[] digits) {
+        boolean needsExtra = true;
+
+        for (int i: digits) {
+            if (i != 9) {
+                needsExtra = false;
+                break;
+            }
+        }
+
+        if (needsExtra) {
+            int[] ret = new int[digits.length + 1];
+            ret[0] = 1;
+            return ret;
+        } else {
+            digits[digits.length-1]++;
+
+            for (int i = digits.length-1; i >= 0; i--) {
+                if (digits[i] == 10) {
+                    digits[i] = 0;
+                    digits[i-1]++;
+                } else return digits;
+            }
+        }
+
         return digits;
     }
 }
