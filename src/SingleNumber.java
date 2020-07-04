f098778ee539545de20f3f42d2a43d37ff44e3dd
Frank <frankyu1999@gmail.com>
2020-07-04 02:10:21 -0400

    SingleNumberV2

--- src/SingleNumber.java
+++ src/SingleNumber.java
@@ -2,8 +2,9 @@ import java.util.*;
 
 public class SingleNumber {
     //V1: using extra memory
-    public int singleNumber(int[] nums) {
-
+    //Runtime: 11 ms
+    //Memory usage: 46.5 MB
+    public int singleNumberV1(int[] nums) {
         HashMap<Integer, Integer> visited = new HashMap<>();
 
         for (int i: nums) {
@@ -18,5 +19,15 @@ public class SingleNumber {
             if (e.getValue() == 1) return e.getKey();
         }
         return 0;
+    }
+
+    //V2: no extra memory, much better runtime. Actually proud I figured this out...
+    //Runtime: 1 ms
+    //Memory: 41 MB
+    public int singleNumber(int[] nums) {
+         for(int i = 1; i < nums.length; i++) {
+             nums[0] ^= nums[i];
+         }
+         return nums[0];
     }
 }
