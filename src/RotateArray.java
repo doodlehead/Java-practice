6c826dae338cbae75050e3b2483d42dac79863ed
Frank <frankyu1999@gmail.com>
2020-07-19 23:01:17 -0400

    RotateArray V2

--- src/RotateArray.java
+++ src/RotateArray.java
@@ -1,5 +1,9 @@
+import java.util.Arrays;
+
 public class RotateArray {
     //V1: new array, ezpz
+    //Runtime: 1 ms
+    //Memory usage: 39.8 MB
     public void rotateV1(int[] nums, int k) {
         if (k == 0) return;
 
@@ -15,16 +19,61 @@ public class RotateArray {
     }
 
     //V2: rotate it in-place, use constant memory
-//    public void rotate(int[] nums, int k) {
-//        int r = k % nums.length; //No need to rotate more than one rotation
+    //Runtime: 2 ms
+    //Memory usage: 42.5 MB ??? It's worse lol
+    public void rotate(int[] nums, int k) {
+        int r = k % nums.length; //No need to rotate more than one rotation
+
+        if (r == 0 || nums.length < 2) return;
+
+        if (nums.length % r == 0) {
+            //Divisible
+            for (int i = 0; i < r; i ++) {
+                int first = nums[i];
+                int index = i;
+                //Y'know you could technically multi-thread and parallelize this... Not sure if it would be faster though
+                for (int j = 0; j < nums.length/r - 1; j++) {
+                    nums[index] = nums[(index + nums.length - r) % nums.length];
+                    index = (index + nums.length - r) % nums.length;
+                }
+                nums[(i + r) % nums.length] = first;
+            }
+        } else {
+            int limit = nums.length;
+            int startingIndex = 0;
+
+            while (limit > 0) {
+                int first = nums[startingIndex];
+                int index = startingIndex;
+                //Keep going around backwards
+                for (int i = 0; i < limit; i++) {
+                    nums[index] = nums[(index - r + nums.length) % nums.length];
+                    index = (index - r + nums.length) % nums.length;
+
+                    //We've done a cycle
+                    if (index == startingIndex) {
+                        limit -= i; //update the new limit
+                        break;
+                    }
+                }
+                nums[(startingIndex + r) % nums.length] = first;
+                limit--;
+                startingIndex++; //increment starting index
+            }
+        }
+    }
+
+//    public static void main(String[] args) {
+//        RotateArray ra = new RotateArray();
+//        int[] arr = new int[]{ 1, 2, 3, 4, 5, 6 };
 //
-//        if (k == 0) return;
+//        //Test not divisible
+//        ra.rotate(arr, 4);
+//        System.out.println(Arrays.toString(arr));
 //
-//        if (nums.length % k == 0) {
-//            //Divisible
-//
-//        } else {
-//
-//        }
+//        //Test divisible
+//        int[] arr2 = new int[]{ 1, 2, 3, 4, 5, 6 };
+//        ra.rotate(arr2, 2);
+//        System.out.println(Arrays.toString(arr2));
 //    }
 }
